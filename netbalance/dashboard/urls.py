ddb02743c4500334e23649758b9b7df55e183144
Danny Vuong <mindarc14@gmail.com>
2023-01-09 23:56:24 -0500

    Delete netbalance directory

--- netbalance/dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        #path('', views.template_base, name='template_base'),
-        path('', views.template_dashboard, name='template_dashboard'),
-        path('settings', views.template_settings, name='template_settings')
-]
