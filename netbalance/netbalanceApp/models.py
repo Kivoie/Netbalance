d6d3876369fb5f43f6570fffd33fd617131aa793
Danny Vuong <mindarc14@gmail.com>
2023-01-13 00:21:00 -0500

    Add files via upload

---
+++ netbalance/netbalanceApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class NewApplication(models.Model):
+    app_name = models.CharField(max_length=64)
+    location = models.CharField(max_length=128)
+    ip = models.CharField(max_length=15)
+    date = models.CharField(max_length=8)
+    requires_provision = models.BooleanField()
+    description = models.CharField(max_length=128)
