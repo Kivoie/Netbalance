7fe86f01c3e49c287954d1bc1f592cbea29b5dc5
Alex Turner <alextis@hotmail.com>
2023-04-06 18:38:58 -0400

    Added cluster reset and changed formatting

--- netbalance/netbalanceApp/static/javascript/dashboardv2.js
+++ netbalance/netbalanceApp/static/javascript/dashboardv2.js
@@ -3,20 +3,12 @@
 // add listen events for the buttons
 document.getElementById("add").addEventListener("click", () => {
     document.getElementById("remove-card").style.display = "none";
-    document.getElementById("upload-card").style.display = "none";
     document.getElementById("add-card").style.display = "block";
 });
 
 document.getElementById("remove").addEventListener("click", () => {
-    document.getElementById("upload-card").style.display = "none";
     document.getElementById("add-card").style.display = "none";
     document.getElementById("remove-card").style.display = "block";
-});
-
-document.getElementById("upload").addEventListener("click", () => {
-    document.getElementById("remove-card").style.display = "none";
-    document.getElementById("add-card").style.display = "none";
-    document.getElementById("upload-card").style.display = "block";
 });
 
 document.getElementById("close-1").addEventListener("click", () => {
@@ -26,12 +18,6 @@ document.getElementById("close-1").addEventListener("click", () => {
 document.getElementById("close-2").addEventListener("click", () => {
     document.getElementById("remove-card").style.display = "none";
 });
-
-document.getElementById("close-3").addEventListener("click", () => {
-    document.getElementById("upload-card").style.display = "none";
-});
-
-
 
 // Add event listeners to the button
 showPasswordButton.addEventListener('mousedown', function() {
